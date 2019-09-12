5af54807b9c76329543d90c6a9e5df31c59ed62d
Tyler Gilchrist <tyler668@gmail.com>
2019-09-12 15:37:01 -0400

    dependencies, testing api

---
+++ xkcd-api/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {useEffect} from 'react';
+import Axios from 'axios';
+
+function App() {
+
+  useEffect(() => {
+    Axios.get(`https://cors-anywhere.herokuapp.com/https://xkcd.com/614/info.0.json`)
+    .then(res =>{
+       console.log(res.data)
+    })
+   
+}, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// let url = '';
+// fetch(url)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Something broke');
+//         }
+//         return response.json();
+        
+//     })
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
