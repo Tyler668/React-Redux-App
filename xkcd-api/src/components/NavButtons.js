8749409e91e89c01a03ceffa540430f8db9b36d4
Tyler Gilchrist <tyler668@gmail.com>
2019-09-12 18:27:17 -0400

    reformatting button func

--- xkcd-api/src/components/NavButtons.js
+++ xkcd-api/src/components/NavButtons.js
@@ -2,21 +2,14 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getComicAC } from '../actions';
 
-const NavButtons = ({ getComicAC, isFetching, error, number }) => {
-
-
-    useEffect(() => {
-            getComicAC()
-    }, [getComicAC])
-
-
+const NavButtons = (props) => {
 
     return (
         <div className='nav-buttons'>
             <button >Back</button>
-            <button onClick={() => getComicAC(Math.floor(Math.random() * 2202))}>Random</button>
-            <button onClick={() => getComicAC('today')}>Today</button>
-            <button onClick={() => getComicAC(5)}>Next</button>
+            <button onClick={() => props.getComicAC(Math.floor(Math.random() * 2202))}>Random</button>
+            <button onClick={() => props.getComicAC('today')}>Today</button>
+            <button onClick={() => props.getComicAC(props.propsNum + 1)}>Next</button>
         </div>
     );
 }
